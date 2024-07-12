901f1b7565e7d4aa22b9f1c074513bb418283849
nicodevelop <nicolas@nico-develop.com>
2024-07-12 21:58:06 +0200

    chore: #29 - Update draft-related functions to include translation functionality

--- functions/src/shared/firebase/draft.ts
+++ functions/src/shared/firebase/draft.ts
@@ -2,6 +2,7 @@ import { DocumentReference, Firestore } from "firebase-admin/firestore";
 import { Draft, DraftId, SiteId } from "../types";
 import { getSiteByDomain, getSiteById } from "./site";
 import { DRAFT_COLLECTION } from "./types";
+import { Article } from "../types/article";
 
 export const updateDraft = async (draftId: DraftId, siteId: SiteId, data: any, db: Firestore): Promise<void> => {
     const site = await getSiteById(siteId, db);
@@ -10,7 +11,20 @@ export const updateDraft = async (draftId: DraftId, siteId: SiteId, data: any, d
         throw new Error("Site not found");
     }
 
-    await site.ref.collection("drafts").doc(draftId).set(data, { merge: true });
+    await site.ref.collection(DRAFT_COLLECTION).doc(draftId).set(data, { merge: true });
+};
+
+export const updateDraftArticle = async (draftId: DraftId, siteId: SiteId, article: Article, db: Firestore): Promise<void> => {
+    const site = await getSiteById(siteId, db);
+
+    if (!site) {
+        throw new Error("Site not found");
+    }
+
+    await site.ref.collection(DRAFT_COLLECTION).doc(draftId).set({
+        publishableArticle: article,
+        status: 'READY_FOR_PUBLISHING'
+    }, { merge: true });
 };
 
 export const createDraft = async (draft: Draft, db: Firestore): Promise<DocumentReference> => {
