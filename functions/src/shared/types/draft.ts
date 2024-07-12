bd79342d3e97639140673b8dccbc920ea86a59d7
nicodevelop <nicolas@nico-develop.com>
2024-07-12 16:38:15 +0200

    feat: Add status field to Draft type and factory function

--- functions/src/shared/types/draft.ts
+++ functions/src/shared/types/draft.ts
@@ -1,6 +1,17 @@
+/**
+ * The status of a draft.
+ * DRAFT: Le brouillon a été créé mais n'a pas encore été traité.
+ * ARTICLE_CREATED: Le brouillon a été traité et un article a été créé.
+ * SEO_OPTIMIZED: L'article a été optimisé pour le SEO (titre, mots clés, description et extrait).
+ * TRANSLATED: L'article a été traduit dans une autre langue.
+ * READY_FOR_PUBLISHING: L'article est prêt à être publié (Déplacé dans la collection content).
+ */
+export type DraftStatus = 'DRAFT' | 'ARTICLE_CREATED' | 'SEO_OPTIMIZED' | 'TRANSLATED' | 'READY_FOR_PUBLISHING';
+
 export type Draft = {
     siteId: string;
     content: string;
+    status: DraftStatus;
     createdAt: Date;
     updatedAt: Date;
 }
@@ -12,12 +23,14 @@ export function draftFactory(
 export function draftFactory(
     siteId: string,
     content: string,
+    status?: DraftStatus,
     createdAt?: Date,
     updatedAt?: Date
 ): Draft {
     return {
         siteId,
         content,
+        status: status || 'DRAFT',
         createdAt: createdAt || new Date(),
         updatedAt: updatedAt || new Date()
     }
