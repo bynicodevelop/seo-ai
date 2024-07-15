3209ddc566a075545685601ff6b41d1f607f61ba
nicodevelop <nicolas@nico-develop.com>
2024-07-15 18:31:30 +0200

    feat: #4 - Add useHead hook to set canonical link in app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+const { getCanonical } = useUtils();
+
+useHead({
+  link: [
+    {
+      rel: 'canonical',
+      href: getCanonical()
+    }
+  ]
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
