9515243eeaa1faf1e662f9ad0ad876149df4e313
nicodevelop <nicolas@nico-develop.com>
2024-07-25 08:29:39 +0200

    fix: Update fetchCategories function to pass the domain parameter

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,54 +1,52 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
-import { 
-  useI18n, useLocalePath, useNuxtApp, useAsyncData, useContent 
-} from '#imports';
 import type { Category } from '~/functions/src/shared';
 
 const { locale } = useI18n();
 const localePath = useLocalePath();
-const { $translate } = useNuxtApp() as {
-  $translate: (key: string, locale: string) => string;
+const {
+  $domain, $translate
+} = useNuxtApp() as unknown as {
+  $domain: string,
+  $translate: Function
 };
 const { fetchCategories } = useContent();
-
 const categories = ref<Category[]>([]);
 
-const fetchCategoriesData = async () => {
-  try {
-    const { data } = await useAsyncData<Category[]>(
-      'categories',
-      async () => await fetchCategories()
-    );
-    categories.value = data.value || [];
-  } catch (error) {
-    console.error(
-      'Failed to fetch categories:',
-error
-);
-    categories.value = [];
-  }
-};
+try {
+  const { data } = await useAsyncData<Category[]>(
+    'categories',
+    async () => await fetchCategories($domain)
+  );
 
-fetchCategoriesData();
+  categories.value = data.value || [];
+} catch (error) {
+  console.log(error);
+
+  throw createError({
+    statusCode: 404,
+    message: 'Category not found',
+    fatal: true
+  });
+}
 </script>
 
 <template>
-  <section class="container mx-auto space-y-4">
-    <h1>{{ $t('pages.categories.title') }}</h1>
-    <div v-if="categories && categories.length > 0" class="space-y-4">
+  <main class="container mx-auto space-y-4">
+    <header>
+      <h1>{{ $t('pages.categories.title') }}</h1>
+    </header>
+    <section v-if="categories && categories.length > 0" class="space-y-4">
       <div v-for="(category, index) in categories" :key="index" class="p-4 border rounded-lg">
         <h2 class="text-lg font-semibold">
-          <NuxtLink :to="localePath(`/categories/${category.slug}`)">
+          <NuxtLink :to="localePath(`/categories/${$translate(category.slug, locale)}`)">
             {{ $translate(category.title, locale) }}
           </NuxtLink>
         </h2>
         <p>{{ $translate(category.description, locale) }}</p>
       </div>
-    </div>
-    <div v-else>
+    </section>
+    <section v-else>
       <p>{{ $t('pages.categories.no_categories') }}</p>
-    </div>
-  </section>
+    </section>
+  </main>
 </template>
